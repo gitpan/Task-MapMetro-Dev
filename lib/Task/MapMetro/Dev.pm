66a5a0b45a31884bae133e5b01dca54fd3917247
Erik Carlsson <info@code301.com>
2015-01-04 18:06:50 +0000

    Import of CSSON/Task-MapMetro-Dev-0.1000.tar.gz from CPAN.
    gitpan-cpan-distribution: Task-MapMetro-Dev
    gitpan-cpan-version:      0.1000
    gitpan-cpan-path:         CSSON/Task-MapMetro-Dev-0.1000.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

---
+++ lib/Task/MapMetro/Dev.pm
@@ -0,0 +1,74 @@
+package Task::MapMetro::Dev;
+
+our $VERSION = '0.1000'; # VERSION
+
+1;
+
+# ABSTRACT: Everything useful when developing Map::Metro maps
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::MapMetro::Dev - Everything useful when developing Map::Metro maps
+
+=head1 VERSION
+
+version 0.1000
+
+=head1 SYNOPSIS
+
+	# install graphviz.
+	# eg:
+	$ sudo apt-get install graphviz
+
+	# and then
+    $ cpanm Task::MapMetro::Dev
+
+=head1 TASK CONTENTS
+
+=head2
+
+=head3 L<Map::Metro> 0.2002
+
+=head3 L<GraphViz2> 2.20
+
+=head3 L<Dist::Zilla> 5.000
+
+=head3 L<Dist::Zilla::MintingProfile::MapMetro::Map> 0.1200
+
+=head3 L<Dist::Zilla::Plugin::MapMetro::MakeGraphViz> 0.1100
+
+=head1 SEE ALSO
+
+L<Map::Metro>
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015 - Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Erik Carlsson <info@code301.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Erik Carlsson.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
